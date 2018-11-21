efee99b03f5e1707d641fae79413cddf3240c0ac
augusto urga <augusto.urga@mercadolibre.com>
2018-11-21 19:40:18 -0300

    pace gato

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
 const http = require('http');
 const mqtt = require('mqtt');
-const config =require('./config');
+const config = require('./config');
 const hostname = '127.0.0.1';
 const mqttServer = 'mqtt://m15.cloudmqtt.com';
 const port = 3000;
 var options = {
-  port:13049,
+  port: 13049,
   host: mqttServer,
   username: 'ipwqlqlt',
   password: '62b_mnAEIeNB'
 }
-var client  = mqtt.connect('mqtt://m15.cloudmqtt.com',options)
+var client = mqtt.connect('mqtt://m15.cloudmqtt.com', options)
 var topics = {
-  config : 'sear/1/config'
+  config: 'sear/1/test',
+  caca: 'sear/1/caca'
 }
-var initMessage = '{"action":"sear/1/config", "sM": 80, "lOn": 21, "lOff": 22, "vF": 21, "vD": 22, "vOn": 21, "vOff": 22}\n'
+var initMessage = '{"action":"sear/1/test", "sM": 80, "lOn": 21, "lOff": 22, "vF": 21, "vD": 22, "vOn": 21, "vOff": 22}\n'
 
-
-client.on('connect', function () {
-  client.subscribe(topics.config, function (err) {
-    if (!err) {
-      client.publish(topics.config,initMessage)
-    }else {
-    }
-  })
-})
- 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -40,4 +26,22 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  turnOnTopics();
+  listenTopics();
 });
+
+turnOnTopics = () => {
+  client.on('connect', function () {
+    client.subscribe(topics.config, function (err) {});
+    client.subscribe(topics.caca, function (err) {})
+
+  })
+}
+
+listenTopics = () => {
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log("topic",topic.toString());
+    console.log("message",message.toString());
+  })
+}
