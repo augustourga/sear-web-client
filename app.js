6557edda5e6171323370dcef1807acfe2a00601a
apace <apace@jamcity.com>
2018-11-24 20:31:34 -0300

    config view envia valores a app.js por POST

--- app.js
+++ app.js
@@ -19,27 +19,46 @@ var topics = {
 }
 var initMessage = '{"action":"sear/1/test", "sM": 80, "lOn": 21, "lOff": 22, "vF": 21, "vD": 22, "vOn": 21, "vOff": 22}\n'
 
+var response = JSON.parse(initMessage);
+
+
 var app = express();
 app.use(express.static(__dirname + '/'));
 app.set('views', __dirname + '/views');
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs');
 
+//To read POST values
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 
 // index page 
 app.get('/', function(req, res) {
+  res.render('pages/index',{ventilador:"test"});
+
+});
+
+app.get('/index', function(req, res) {
 	res.render('pages/index');
 });
 
-// about page 
-app.get('/config', function(req, res) {
-	res.render('pages/config');
-});
-
+// info page 
 app.get('/info', function(req, res) {
 	res.render('pages/info');
 });
 
+
+// config page 
+app.get('/config', function(req, res) {
+  res.render('pages/config');
+});
+
+//read POST values from views /
+app.post('/', function(req, res) {
+  console.log(req.body );
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
@@ -58,7 +77,19 @@ turnOnTopics = () => {
 listenTopics = () => {
   client.on('message', function (topic, message) {
     // message is Buffer
-    console.log("topic",topic.toString());
-    console.log("message",message.toString());
+    // console.log("topic",topic.toString());
+    
+    try
+    {
+      var response = JSON.parse(message.toString());
+      console.log("Receivine message from :", response.action); //Topic Name
+    }
+    catch(e)
+    {
+      Console.log("Error getting topic answer",response);
+    }
+    
+
+
   })
 }
